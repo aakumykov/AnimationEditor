02e4ecfb29603d494389550acd1d67f6fb3ec8b5
Андрей Кумыков <aakumykov@yandex.ru>
2016-12-05 15:49:12 +0700

    Непродуманное начало с sender.

--- www/js/index.js
+++ www/js/index.js
@@ -10,4 +10,5 @@ $(document).ready(function(){
     );
 
     page.init('#toolbar', '#editor');
+    sender.init('#toolbar');
 });
---
+++ www/js/sender/sender.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var sender = {
+	init: function(buttonParentId) {
+		$(buttonParentId).append(
+			$("<button id='sender_button'><i class='fa fa-arrow-right></i></button>")
+		);
+	},
+
+
+}
